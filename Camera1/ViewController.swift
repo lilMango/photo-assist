57ce818eea49e3e800a1a6bce5b10853d71c024d
Miguel Paysan Mac <miguelpaysan@gmail.com>
2015-12-28 12:09:46 -0800

    Initial Commit

---
+++ Camera1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Camera1
+//
+//  Created by Miguel Paysan on 12/28/15.
+//  Copyright © 2015 Miguel Paysan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
