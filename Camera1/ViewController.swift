fc57cae6c872b9d425062b8e36659ed948cbe3dc
Miguel Paysan Mac <miguelpaysan@gmail.com>
2016-01-04 21:42:08 -0800

    Getting Camera working with preview in the same View

--- Camera1/ViewController.swift
+++ Camera1/ViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
 
+    var captureSession: AVCaptureSession?
+    var stillImageOutput: AVCaptureStillImageOutput?
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,67 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(animated: Bool) {
+        captureSession = AVCaptureSession()
+        captureSession!.sessionPreset = AVCaptureSessionPresetPhoto
+        
+        var backCamera = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        let input = try! AVCaptureDeviceInput(device: backCamera)
+        
+        if (captureSession!.canAddInput(input)) {
+            captureSession!.addInput(input)
+            print("Added Input to session")
+        }else {
+            print("could NOTT add Input to session")
+        }
+        
+        stillImageOutput = AVCaptureStillImageOutput()
+        stillImageOutput!.outputSettings = [AVVideoCodecKey: AVVideoCodecJPEG]
+
+        if captureSession!.canAddOutput(stillImageOutput) {
+            captureSession!.addOutput(stillImageOutput)
+            
+            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            previewLayer!.videoGravity = AVLayerVideoGravityResizeAspectFill
+            previewLayer!.connection?.videoOrientation = AVCaptureVideoOrientation.Portrait
+            previewView.layer.addSublayer(previewLayer!)
+            
+            captureSession!.startRunning()
+        } else {
+            print("Failed at add Output to capture Session")
+        }
+        
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        previewLayer!.frame = previewView.bounds
+        
+    }
+    
+    @IBOutlet weak var previewView: UIView!
+    
+
+    @IBOutlet weak var capturedImage: UIImageView!
+
+    @IBAction func didPressTakePhoto(sender: UIButton) {
+        
+        if let videoConnection = stillImageOutput!.connectionWithMediaType(AVMediaTypeVideo) {
+            videoConnection.videoOrientation = AVCaptureVideoOrientation.Portrait
+            stillImageOutput?.captureStillImageAsynchronouslyFromConnection(videoConnection, completionHandler: {(sampleBuffer, error) in
+                if (sampleBuffer != nil) {
+                    let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
+                    let dataProvider = CGDataProviderCreateWithCFData(imageData)
+                    let cgImageRef = CGImageCreateWithJPEGDataProvider(dataProvider, nil, true, CGColorRenderingIntent.RenderingIntentDefault)
+                    
+                    let image = UIImage(CGImage: cgImageRef!, scale: 1.0, orientation: UIImageOrientation.Right)
+                    self.capturedImage.image = image
+                }
+            })
+        }
+
+    }
 
 }
 
