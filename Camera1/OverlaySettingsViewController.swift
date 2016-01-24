8368868180c245130c9b3237e960165e8b90458e
Miguel Paysan Mac <miguelpaysan@gmail.com>
2016-01-24 00:53:56 -0800

    tab navigation with empty controllers. TODO draw out what Overlay Settings screen does

---
+++ Camera1/OverlaySettingsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  OverlaySettingsViewController.swift
+//  Camera1
+//
+//  Created by Miguel Paysan on 1/24/16.
+//  Copyright © 2016 Miguel Paysan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class OverlaySettingsViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+    
+    }
+    
+    
+}
